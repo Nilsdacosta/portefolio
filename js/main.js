3f71360c441ec61be13fdf6c76d4226a5a721bb8
Nilsdacosta <nilsdacosta@gmail.com>
2020-03-22 15:28:22 +0100

    ajout hover homepage

--- js/main.js
+++ js/main.js
@@ -2,4 +2,29 @@
 
 $(document).ready(function(){
     apparition();
+
+    // var maNavigation = $("article").mouseenter(function(){
+    //     return nomNavigation = "#" + this.id;
+    // });
+    // // fonction qui va changer l'apparence de mes nav
+    // $(maNavigation).mouseenter(function(){
+    //     console.log(nomNavigation);
+    
+    //     if(nomNavigation == "#competence")
+    //     {
+    //         $('#competence').css('background-image', 'linear-gradient(-225deg, rgba(0, 0, 0, 0.6)25%, rgb(157, 199, 207, 0.9)75%), url("../image/fondQuatre.jpg")');
+    //     }else if(nomNavigation == "#formation")
+    //     {
+    //         $("#pageFormation").fadeIn();
+    //     }else if(nomNavigation == "#experience")
+    //     {
+    //         $("#pageExperience").fadeIn();
+    //     }else if(nomNavigation == "#projet")
+    //     {
+    //         $("#pageProjet").fadeIn();
+    //     }else
+    //     {
+    //         alert("Erreur");
+    //     }
+    // });
 });
